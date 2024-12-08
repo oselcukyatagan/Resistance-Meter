4caa4ca903142d493ce69b69cef12581eef01220
oselcukyatagan <115888665+oselcukyatagan@users.noreply.github.com>
2024-12-09 01:12:42 +0300

    Update meter.ino

--- meter.ino
+++ meter.ino
@@ -3,81 +3,59 @@
 // LiquidCrystal lcd(Rs, Enable, 4, 5, 6, 7);
 
 LiquidCrystal lcd(9, 8, 4, 5, 6, 7);
-
 int lcd_setup_time = 1000;
+
+const double VOLTAGE_THRESHOLD = 4.8;
 
 String str = "Resistance Meter ";
 
-
 int volt_1023;
-
 float converter = (5./1023.);
-
 float volt_real;
-
 
 int measure = A5;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  
+    pinMode(measure, INPUT);
 
-  pinMode(measure, INPUT);
-
-  lcd.begin(16, 2);
-  lcd.setCursor(0,0);
-  lcd.print(str);
- 
-
-  delay(lcd_setup_time);
- 
+    lcd.begin(16, 2);
+    lcd.setCursor(0,0);
+    lcd.print(str);
+    
+    delay(lcd_setup_time);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+    lcd.setCursor(0, 1);
+    lcd.print("                "); // 16 spaces to clear the row
 
-  for(int i = 0; i < 3; i++){
-    lcd.setCursor(i,1);
-    lcd.print(" ");
-  }
+    lcd.setCursor(0, 1); 
 
-  lcd.setCursor(0, 1); 
-  volt_1023 = analogRead(measure);
-  volt_real = converter * volt_1023; // Map the analog reading to a voltage range
-  int resistance = (volt_real * 1000) / (5 - volt_real);
+    volt_1023 = analogRead(measure);
+    volt_real = converter * volt_1023; // Map the analog reading to a voltage range
 
+    if(volt_real > VOLTAGE_THRESHOLD){
+        lcd.print("Change to 100k.");
+        return;
+    }
 
-  Serial.println(volt_real * 1000);
-  Serial.println(5 - volt_real);
-  Serial.println(resistance);
-   
+    int resistance = (volt_real * 1000) / (5.0 - volt_real);
+    lcd.setCursor(0, 1); // Set the cursor to the second row of the LCD
 
-  lcd.setCursor(0, 1); // Set the cursor to the second row of the LCD
-
-
-  if(resistance > 1000){  
-    double _resistance = resistance / 1000.0;
     lcd.print("V:");
     lcd.print(volt_real, 2); // Display the voltage on the LCD with 2 decimal places
     lcd.print(" R:");
-    lcd.print(_resistance,2);
-    lcd.print("k");
-    lcd.setCursor(14,1);
-    lcd.print("     ");
-  }
-  else{
-    lcd.print("V:");
-    lcd.print(volt_real, 2); // Display the voltage on the LCD with 2 decimal places
-    lcd.print(" R:");
-    lcd.print(resistance);
-    lcd.setCursor(12,1);
-    lcd.print("     ");
-  }
-
-  
-  delay(500); // Delay for readability, adjust as needed
-  
+    if(resistance > 1000){  
+        double _resistance = resistance / 1000.0;
+        lcd.print(_resistance,2);
+        lcd.print("k");
+    }
+    else
+        lcd.print(resistance);
+    
+    delay(500); // Delay for readability, adjust as needed
 }
